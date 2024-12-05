6f38786489176e36c0ec86ee32b92984c4201ef6
alfajorkum <mcamilla20@alumnos.utalca.cl>
2024-12-05 14:57:02 -0300

    Actualizaod

--- DjangoApi/DjangoApi/urls.py
+++ DjangoApi/DjangoApi/urls.py
@@ -3,8 +3,8 @@ from django.urls import path, include
 from rest_framework.documentation import include_docs_urls
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("", include("api.urls")),
-    path("", include("usuarios.urls")),
-    path("docs/", include_docs_urls(title="api ayudantias")),
+    path("backend/admin/", admin.site.urls),
+    path("backend/", include("api.urls")),
+    path("backend/", include("usuarios.urls")),
+    path("backend/docs/", include_docs_urls(title="api ayudantias")),
 ]
