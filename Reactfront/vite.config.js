5bbe74ae6e507d473c27a4b5619fbe8e8b5f56d3
alfajoruwu <mcamilla20@alumnos.utalca.cl>
2025-03-12 08:59:41 -0300

    Funciona el front

--- Reactfront/vite.config.js
+++ Reactfront/vite.config.js
@@ -4,5 +4,9 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/ayudantias',
+  base: '/ayudantias/',
+  build: {
+    outDir: 'dist', // Directorio de salida local
+    assetsDir: 'assets' // Carpeta para recursos estáticos
+  }
 })
